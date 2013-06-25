9812661ee270ccaf6806ccccd34f0b4d2504c763
David Fisher <tibbon+github@gmail.com>
2013-06-25 11:50:37 -0400

    ABout to add views and templating

--- main.rb
+++ main.rb
@@ -27,12 +27,25 @@ get '/names/:first/:last' do
           #{@first.capitalize}, #{@last.capitalize}!"
 end
 
+# Sums two numbers from the URL
 # localhost:4567/calc/add/1/2
 get '/calc/add/:first/:second' do
   #passes in params as strings. Must change to integer
   @first = params[:first].to_i
   @second = params[:second].to_i
-
   #It wants a string to output it again
   return (@first + @second).to_s
+end
+
+#Returns the product of two numbers
+# localhost:4567/calc/multiply/5/10
+get '/calc/multiply/:first/:second' do
+  @first = params[:first].to_i
+  @second = params[:second].to_i
+  return (@first * @second).to_s
+end
+
+get '/oh_hai/:name' do
+  @name = params[:name]
+  erb :hai
 end

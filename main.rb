a4e158ee6c78573ec0e9152de75b7d7d8e04a6b8
David Fisher <tibbon+github@gmail.com>
2013-06-25 11:16:46 -0400

    Created 4 examples of using strings and outputting from Sinatra

--- main.rb
+++ main.rb
@@ -4,3 +4,25 @@ require 'sinatra/reloader' if development?
 get '/' do
   "Hello!"
 end
+
+get '/hello_world' do
+  "Hello, World!"
+end
+
+get '/hello_me' do
+  @me = "David"
+  return "Hello, #{@me}"
+end
+
+get '/hello_you/:name' do
+  @name = params[:name]
+  return "Hello, #{@name}"
+end
+
+# localhost:4567/names/david/fisher
+get '/names/:first/:last' do
+  @first = params[:first]
+  @last = params[:last]
+  return "You can do multiple parameters,
+          #{@first.upcase}, #{@last}!"
+end

ebc91c076c80441cf15504974ba6ffd58e204aa9
Saad Rhoulam <saad@rhoulam.com>
2015-06-19 10:25:05 -0400

    Substitute new GH OAuth secret.

--- config/passport.js
+++ config/passport.js
@@ -9,7 +9,7 @@ function configurePassport(mongoose) {
 
 	passport.use(new GithubStrategy({
 			clientID : '01bea1cd4e583bf97a9d', // get these from Github
-			clientSecret : '67934eb0ec4f3d67af08e0037091c910b4e224f9', // get these from Github
+			clientSecret : '    d689ed9ed0f245aa351e4cdf42e09fc537bbc408', // get these from Github
 			callbackURL : "https://ga-wdi-passport-github.herokuapp.com/login/callback"
 		}, function(accessToken, refreshToken, profile, done) {
 			// see https://github.com/jaredhanson/passport-github

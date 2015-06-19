f70ff197d222bb87c02525246bff5e0e666d1cb3
Saad Rhoulam <saad@rhoulam.com>
2015-06-19 10:23:15 -0400

    Note that GH OAuth credentials have been revoked.

--- config/passport.js
+++ config/passport.js
@@ -8,6 +8,7 @@ function configurePassport(mongoose) {
 	var User = mongoose.model('User');
 
 	passport.use(new GithubStrategy({
+			// credentials have since been revoked
 			clientID : '01bea1cd4e583bf97a9d', // get these from Github
 			clientSecret : '67934eb0ec4f3d67af08e0037091c910b4e224f9', // get these from Github
 			callbackURL : "https://ga-wdi-passport-github.herokuapp.com/login/callback"

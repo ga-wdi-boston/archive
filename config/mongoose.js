c640c23b62d1ddf266c8d412d4048b7b8be14812
Saad Rhoulam <saad@rhoulam.com>
2015-06-19 10:21:23 -0400

    Set mongoose URI to heroku mongolab URI

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,4 +1,4 @@
 module.exports = {
-	url : "mongodb://localhost/authapp"
+	url : "mongodb://heroku_j7m65v1w:sevh7gj79prct7gdbvo9on4mup@ds047742.mongolab.com:47742/heroku_j7m65v1w"
 //	options : {}
 };
